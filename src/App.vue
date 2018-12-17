9f5f8821c507a6aeea80fbf2adc757c9582792f5
divya <hello@shortdiv.com>
2018-12-17 09:40:45 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <QuestionForm />
+  </div>
+</template>
+
+<script>
+import QuestionForm from './components/QuestionForm.vue';
+
+export default {
+  name: "app",
+  components: {
+    QuestionForm
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
+</style>
